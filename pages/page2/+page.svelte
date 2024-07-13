6532d9a50ba691f569091fa70c329fa72dd42759
Wrench56 <dmarkreg@gmail.com>
2024-07-12 21:59:40 -0400

    Add `page2`

---
+++ pages/page2/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import AnotherWidget from "$lib/plugins/A_PluginTemplate/folder/AnotherWidget.svelte";
+</script>
+
+<AnotherWidget />
+
+<style>
+  :root {
+    background-color: black;
+  }
+</style>
