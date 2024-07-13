f004d7acd2854ffcad0bd73b4378ee928f9b2abc
Wrench56 <dmarkreg@gmail.com>
2024-07-12 21:52:25 -0400

    Add example page

---
+++ pages/page/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import ExampleWidget from "$lib/plugins/A_PluginTemplate/ExampleWidget.svelte";
+</script>
+
+<ExampleWidget />
+
+<style>
+  :root {
+    background-color: black;
+  }
+</style>
